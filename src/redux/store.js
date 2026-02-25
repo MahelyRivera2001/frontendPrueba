2474204dbb0def523ca682e5c642bb22a61b6699
mahelyrivera <92773054+MahelyRivera2001@users.noreply.github.com>
2026-02-25 11:46:01 -0600

    Creación Estructura base redux
    se crean archivos base para redux, así mismo se agrega la estandarización de mensajes globales que utilizará la aplicación.

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+//configuración store base
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer:{
+
+    },
+});
---
+++ src/utilities/messages.js
@@ -0,0 +1,16 @@
+const messages = {
+    message_400 : "Hubo un problema con los datos enviados",
+    message_404: "Recurso no encontrado",
+    message_500: "Error interno del servidor",
+    message_503: "Servicio no disponible",
+    message_401: "No tiene permisos para acceder a este recurso",
+    message_403: "Acceso denegado",
+    message_409: "La solicitud ha causado una conflicto con el estado actual del recurso",
+    message_default: "Ha ocurrido un error",
+    message_network_error: "Error de conexión",
+    message_exito_guardado: "Se ha agregado con éxito",
+    message_exito_editado: "Se ha editado con éxito",
+    message_exito_eliminado: "Se ha eliminado con éxito",    
+}
+
+export default messages;
