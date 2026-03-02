7262b56e63b4e70d20036479ce503a7c989a12f8
mahelyrivera <92773054+MahelyRivera2001@users.noreply.github.com>
2026-03-02 09:07:44 -0600

    Update Store para reducer (registro)
    se añade el reducer registro al store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,12 @@ import { configureStore } from "@reduxjs/toolkit";
 //reducers
 import ColaboradorReducer from "./mantenimiento/colaborador/colaborador";
 import VehiculoReducer from "./mantenimiento/vehiculo/vehiculo";
+import RegistroReducer from "./registros/registro";
 
 export const store = configureStore({
     reducer:{
         colaborador: ColaboradorReducer,
         vehiculo : VehiculoReducer,
+        registro : RegistroReducer,
     },
 });
